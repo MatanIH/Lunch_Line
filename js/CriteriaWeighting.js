ba2062bea562293d2e41852a2d6a0c2d726f2214
Aidan Fike <aidanfike@students.berkeley.net>
2016-05-23 11:02:56 -0700

    hhgh

--- js/CriteriaWeighting.js
+++ js/CriteriaWeighting.js
@@ -1,65 +1,63 @@
 /**
  * Created by h205p2 on 5/11/16.
  */
-/*    ArrayList<Restaurant> recommendedRestaurants = new ArrayList<>();
-
-           @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-}
+var recommendedRestaurants = [];
+var recents = [];
+var bannedList = [];
+var recRestaurant = true;
+var maxDistance; //user input
 
     if (restaurant.distance > maxDistance)
     {
-        recommendRestaurant = false;
+        restaurant.recRestaurant = false;
     }
-    ArrayList<String> bannedList = new ArrayList<String>();
-    for(Restaurant restaurant : bannedList)
+
+    if (restaurant in bannedList)
     {
-        if (restaurant.equals(theotherone))
-        {
-            recommendRestaurant = false;
-        }
+        restaurant.recRestaurant = false;
     }
-    if (restaurant in ArrayList of those recommended in last 4-5 days)
+
+    if (restaurant in recents)
     {
-        recommendRestaurant = false;
+        restaurant.recRestaurant = false;
     }
 
     if (restaurant.recRestaurant = true)
     {
-        recommendedRestaurants.append[restaurant];
+        recommendedRestaurants.push(restaurant);
     }
 
-    if (multiple restuarants meet criteria)
+    for (k = 0; k < recommendedRestaurants.size(); k++)
     {
-        restaurant1.want += restaurant1.distance + restaurant1.avgPrice;
-        restaurant2.want += restaurant2.distance + restaurant2.avgPrice;
-        //make sure want is an object in restaurant class
-    ...
-        for(int i = 0; i < recommendedRestaurants.size(); i++) {
-            for(int j = 0; j < recommendedRestaurants.size(); j++) {
-                if(recommendedRestaurants[i].wantValue > recommendedRestaurants[j].wantValue) {
+        recommendedRestaurants[k].notWant = recommendedRestaurants[k].distance + recommendedRestaurants[k].lineWait + recommendedRestaurants[k].avgPrice;
+
+
+        for(i = 0; i < recommendedRestaurants.size(); i++) {
+            for(j = 0; j < recommendedRestaurants.size(); j++) {
+                if(recommendedRestaurants[i].want > recommendedRestaurants[j].want) {
                     //insert j before recommendedRestaurants[i]
-                    recommendedRestaurants.add(i-1, recommendedRestaurants[j]);
+                    recommendedRestaurants.push(i-1, recommendedRestaurants[j]);
                 }
             }
         }
     }
-*/
+
     //User-Input Factors: money, distance, time, blocked restaurants, recommended in last 4 or 5 days, raining
     //everytime restaurant recommended and user confirms, add it to ArrayList of recommended restaurants
     //and remove oldest restaurant and move all items over by 1 index
     //have button on recommended restaurant page that allows user to ban restaurant from being recommended again
     //have default distance that goes from university to ____
 
-Restaurant(distance, lineWait, avgPrice, want)
+Restaurant(distance, lineWait, avgPrice)
 {
     this.distance = distance;
-    this.lineWait = lineWait;
+    this.lineWait = lineWait; // make lineWait have three options (Short, Medium, Long) that are associated with numbers of significant difference
     this.isBanned = false;
     this.avgPrice = avgPrice;
-    this.want = want;
+    this.want = 0;
     this.recRestaurant = true;
-    this.isCool = function(){return "yes";}
+    //this.isCool = function(){return "yes";}
 }
+
+var bongo = new Restaurant()
+var sliver = new Restaurant()
